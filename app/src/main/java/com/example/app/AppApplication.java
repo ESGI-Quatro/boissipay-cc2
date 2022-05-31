aad352c1d2b00cc26270d6c5397b6fe80013a56d
Florian Gustin <florian.gustin.business@gmail.com>
2022-05-31 10:14:08 +0200

    feat : bootable

--- app/src/main/java/com/example/app/AppApplication.java
+++ app/src/main/java/com/example/app/AppApplication.java
@@ -3,5 +3,8 @@ package com.example.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-public class AppApplication { }
+@SpringBootApplication
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }}
