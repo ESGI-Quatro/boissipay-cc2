821081eb168f5a9835fcee672105444209a8e544
Florian Gustin <florian.gustin.business@gmail.com>
2022-07-09 17:29:51 +0200

    dir

--- app/src/main/java/com/example/app/payment/exposition/CustomPaymentApiDelegate.java
+++ app/src/main/java/com/example/app/payment/exposition/CustomPaymentApiDelegate.java
@@ -1,30 +1,30 @@
-package com.example.app.payment.exposition;
-
-import com.example.app.payment.application.CreatePaymentTransaction;
-import com.example.app.payment.infrastructure.BoissiPaymentService;
-import com.example.app.payment.infrastructure.JedisCacheService;
-import com.example.app.payment.infrastructure.PaymentInMemoryRepository;
-import com.example.api.PaymentApiDelegate;
-import com.example.model.PaymentRequest;
-import com.example.model.PaymentResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-@Service
-public final class CustomPaymentApiDelegate implements PaymentApiDelegate {
-    @Override
-    public ResponseEntity<PaymentResponse> pay(PaymentRequest body) {
-        PaymentResponse paymentResponse = new PaymentResponse();
-        try {
-            final CreatePaymentTransaction createPaymentTransaction = new CreatePaymentTransaction(new JedisCacheService(), new BoissiPaymentService(), new PaymentInMemoryRepository());
-            final String transactionId = createPaymentTransaction.execute(body.getUserId(), body.getAmount().doubleValue());
-            paymentResponse.transactionId(transactionId);
-            paymentResponse.status(PaymentResponse.StatusEnum.APPROVED);
-            return ResponseEntity.ok(paymentResponse);
-        }catch (Exception e) {
-            paymentResponse.status(PaymentResponse.StatusEnum.REFUSED);
-            return ResponseEntity.internalServerError().body(paymentResponse);
-        }
-
-    }
-}
+//package com.example.app.payment.exposition;
+//
+//import com.example.app.payment.application.CreatePaymentTransaction;
+//import com.example.app.payment.infrastructure.BoissiPaymentService;
+//import com.example.app.payment.infrastructure.JedisCacheService;
+//import com.example.app.payment.infrastructure.PaymentInMemoryRepository;
+//import com.example.api.PaymentApiDelegate;
+//import com.example.model.PaymentRequest;
+//import com.example.model.PaymentResponse;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public final class CustomPaymentApiDelegate implements PaymentApiDelegate {
+//    @Override
+//    public ResponseEntity<PaymentResponse> pay(PaymentRequest body) {
+//        PaymentResponse paymentResponse = new PaymentResponse();
+//        try {
+//            final CreatePaymentTransaction createPaymentTransaction = new CreatePaymentTransaction(new JedisCacheService(), new BoissiPaymentService(), new PaymentInMemoryRepository());
+//            final String transactionId = createPaymentTransaction.execute(body.getUserId(), body.getAmount().doubleValue());
+//            paymentResponse.transactionId(transactionId);
+//            paymentResponse.status(PaymentResponse.StatusEnum.APPROVED);
+//            return ResponseEntity.ok(paymentResponse);
+//        }catch (Exception e) {
+//            paymentResponse.status(PaymentResponse.StatusEnum.REFUSED);
+//            return ResponseEntity.internalServerError().body(paymentResponse);
+//        }
+//
+//    }
+//}
